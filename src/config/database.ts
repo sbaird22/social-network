65deb148e0afcd645ee3591c5275280e41b1f016
sbaird22 <shanebaird30@yahoo.com>
2025-01-08 21:54:36 -0700

    just some debugging

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
-try {
-    await mongoose.connect(process.env.MONGODB_URI || '');
-    console.log('MongoDB connected...');
-} catch (err) {
-    console.error('Database connection failed:', err);
+    try {
+    const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+    await mongoose.connect(mongoURI);
+    console.log('MongoDB connected successfully.');
+    } catch (err) {
+    console.error('Failed to connect to MongoDB:', err);
     process.exit(1);
-}
+    }
 };
 
 export default connectDB;
